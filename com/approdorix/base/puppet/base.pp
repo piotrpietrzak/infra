ae6ca82989e99b358d32c49ecf368b15a9fc2508
Piotr Pietrzak <piotr.pietrzak@4finance.com>
2015-09-05 22:34:00 +0200

    Init - base recurrent docker file to setup infrastructure

---
+++ com/approdorix/base/puppet/base.pp
@@ -0,0 +1,12 @@
+node "base" {
+
+  file { '/test.txt':
+    ensure => "file",
+    owner  => "root",
+    group  => "root",
+    mode   => "700",
+    content => "Congratulations!
+Puppet has created this file.
+",}
+
+}
---
+++ com/approdorix/base/scripts/main.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /home/infra/
+git pull 
+puppet apply ./com/approdorix/base/puppet/base.pp
