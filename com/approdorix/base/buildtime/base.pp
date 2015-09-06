d6be5870702914197ab513b31e28950eb9df1fdb
Piotr Pietrzak <piotr.pietrzak@4finance.com>
2015-09-06 03:18:18 +0200

    WIP

--- com/approdorix/base/buildtime/base.pp
+++ com/approdorix/base/buildtime/base.pp
@@ -1,11 +1,12 @@
-node "base" {
+node default {
 
   file { '/build.txt':
     ensure => "file",
     owner  => "root",
     group  => "root",
     mode   => "700",
-    content => "Setup in buildtime
+    content => "Setup in buildtime! wow
 ",}
+  notify {"Running":}
 
 }
